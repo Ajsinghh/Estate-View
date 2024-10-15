30b8b183996502ce844213d9f476dd7733b99c36
Ajay <ajsinghdhanda0207@gmail.com>
2024-10-15 21:43:15 +0530

    added create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.route.js')
 const authRouter = require('./routes/auth.route.js')
+const listingRouter = require('./routes/listing.route.js')
 const cookieParser = require('cookie-parser')
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.listen(3000,()=> console.log("server is running on port 3000"));
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
 app.use((err, req, res, next)=>{
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error'
