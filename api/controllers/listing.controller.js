248b8848f98af359f33c973d32133297664ab73c
Ajay <ajsinghdhanda0207@gmail.com>
2024-10-17 23:11:01 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,29 +1,45 @@
 const Listing = require("../models/listing.model");
 const errorHandler = require("../utils/error");
 
-const createListing = async(req, res, next) =>{
-    try{
-       const listing = await Listing.create(req.body);
-       return res.status(201).json(listing);
-    }catch(error){
-        next(error);
-    }
-}
+const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const deleteListing = async(req,res, next) =>{
-    const listing =  await Listing.findById(req.params.id);
-    if(!listing){
-        return next(errorHandler(404,'Listing not found!'));
-    }
-    if(req.user.id !== listing.userRef){
-        return next(errorHandler(401, 'You can only delete your own listings!'))
-    }
-    try{
-        await Listing.findByIdAndDelete(req.params.id);
-        res.status(200).json('Listing has been deleted!')
-    }catch(error){
-        next(error)
-    }
-}
+const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = {createListing, deleteListing};
+const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if(!listing){
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if(req.user.id !== listing.userRef){
+    return next(errorHandler(401, "You can only edit your own listing!"));
+  }
+  try{
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body ,{new:true})
+    res.status(200).json(updatedListing)
+  }catch(error){
+    next(error);
+  }
+};
+
+module.exports = { createListing, deleteListing, updateListing };
