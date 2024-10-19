9e6151096b3fabde84747a98da97a29e0d40b09c
Ajay <ajsinghdhanda0207@gmail.com>
2024-10-20 01:17:25 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,111 @@
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-5xl mx-auto">
+      <div>
+        <h1 className="text-3xl font-bold mb-4 text-slate-800">
+          About Estate View
+        </h1>
+        <p className="mb-4 text-slate-700 ">
+          Welcome to Estate View, your one-stop platform for finding the perfect
+          property or renting out your home. Whether you're looking to buy your
+          dream house, rent an apartment, or list your property for sale or
+          lease, Estate View is here to make the process easier and more
+          efficient.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-3xl font-semibold mb-4 text-slate-800">
+          What We Offer:
+        </h2>
+        <ul>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              Browse Properties:
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Explore a wide range of properties available for rent or sale.
+              From cozy apartments to luxurious villas, we have something for
+              everyone.
+            </p>
+          </li>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              List Your Properties:
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Got a house to rent or sell? List your property effortlessly and
+              reach potential buyers or renters instantly.
+            </p>
+          </li>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              Connect with Landlords:
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Communicate directly with landlords to inquire about properties,
+              set up viewings, and negotiate deals.
+            </p>
+          </li>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              User-Friendly Interface
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Our platform is designed with simplicity in mind, making it easy
+              for you to navigate, search, and manage your listings.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1 className="text-3xl font-semibold mb-4 text-slate-800">
+          Our Mission:
+        </h1>
+        <p className="mb-4 text-slate-700 ">
+          At Estate View, our mission is to bridge the gap between property
+          owners and seekers by offering a reliable, transparent, and
+          user-friendly experience. We aim to empower individuals by providing
+          them with the tools they need to make informed decisions in the real
+          estate market.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-3xl font-semibold mb-4 text-slate-800">
+          Why Choose Estate View:
+        </h2>
+        <ul>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              Comprehensive Listings:
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Access to a variety of properties in different locations, with
+              detailed descriptions and images.
+            </p>
+          </li>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              Verified Users:
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Interact with genuine landlords and buyers to ensure smooth and
+              trustworthy transactions.
+            </p>
+          </li>
+          <li>
+            <span className="text-xl font-semibold mb-4 text-slate-800">
+              Secure & Fast:
+            </span>
+            <p className="mb-4 text-slate-700 ">
+              Our platform ensures the privacy and security of your information,
+              along with fast communication between users.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default About
